8cb0c476aa0c7d945452ab9c2eb5365ed56587c4
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2016-12-05 09:27:22 -0600

    Even more styling.

--- scripts/script.js
+++ scripts/script.js
@@ -84,10 +84,12 @@ var triggerTarget = function(buttonName) {
 var buildNav = function() {
   // Constructs navigation buttons for side navigation panel.
   $('nav').append('<button name="prev" class="btn btn-primary">Prev</button>\n');
+  $('nav').append('<div class="btn-group-vertical">\n');
   for (var i = 0; i < classmateArray.length; i++) {
     var el = '<button name="' + i + '" class="btn btn-default">' + classmateArray[i].first_name + '</button>\n';
     $('nav').append(el);
   }
+  $('nav').append('</div>\n');
   $('nav').append('<button name="next" class="btn btn-primary">Next</button>\n');
 };
 
